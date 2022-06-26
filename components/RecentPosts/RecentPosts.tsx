b8d736e63203403f71d4e8ab50aa658bdfabe326
Kacper Malek <kacper@growl.be>
2022-06-27 00:18:59 +0200

    refactor: make use of created Posts component to display posts

--- components/RecentPosts/RecentPosts.tsx
+++ components/RecentPosts/RecentPosts.tsx
@@ -1,6 +1,6 @@
 import type { Post } from "@/pages/index";
 
-import Link from "next/link";
+import Posts from "../Posts";
 
 type RecentPostsProps = {
   posts: Post[];
@@ -13,24 +13,7 @@ export default function RecentPosts({ posts }: RecentPostsProps) {
         Recently Published
       </h2>
 
-      <div className="flex flex-col">
-        {posts.map((post: Post) => (
-          <div key={post.filePath} className="flex flex-col my-2">
-            <Link
-              as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`}
-              href={`/posts/[slug]`}
-            >
-              <a className="text-2xl my-2 font-semibold tracking-tight text-blue-500 hover:text-blue-700 transition-colors duration-500 ease-in-out">
-                {post.data.title}
-              </a>
-            </Link>
-            <p className="text-sm text-black leading-7 dark:text-gray-400 mb-1">
-              {post.data.description}
-            </p>
-            <h5>{post.data.date}</h5>
-          </div>
-        ))}
-      </div>
+      <Posts posts={posts} />
     </div>
   );
 }

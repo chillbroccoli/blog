df6bb7df310b591e80949e84ff62aaebfdca6d17
Kacper Malek <kacper@growl.be>
2022-06-26 13:35:53 +0200

    feat: add _document, import IBM Plex Sans font family, add script enabling to change the theme

---
+++ pages/_document.tsx
@@ -0,0 +1,72 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const themeScript = `
+  let mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+  function updateTheme(savedTheme) {
+    let theme = 'system'
+    try {
+      if (!savedTheme) {
+        savedTheme = window.localStorage.theme
+      }
+      if (savedTheme === 'dark') {
+        theme = 'dark'
+        document.documentElement.classList.add('dark')
+      } else if (savedTheme === 'light') {
+        theme = 'light'
+        document.documentElement.classList.remove('dark')
+      } else if (mediaQuery.matches) {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
+    } catch {
+      theme = 'light'
+      document.documentElement.classList.remove('dark')
+    }
+    return theme
+  }
+
+  function updateThemeWithoutTransitions(savedTheme) {
+    updateTheme(savedTheme)
+    document.documentElement.classList.add('[&_*]:!transition-none')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('[&_*]:!transition-none')
+    }, 0)
+  }
+
+  document.documentElement.setAttribute('data-theme', updateTheme())
+
+  new MutationObserver(([{ oldValue }]) => {
+    let newValue = document.documentElement.getAttribute('data-theme')
+    if (newValue !== oldValue) {
+      try {
+        window.localStorage.setItem('theme', newValue)
+      } catch {}
+      updateThemeWithoutTransitions(newValue)
+    }
+  }).observe(document.documentElement, { attributeFilter: ['data-theme'], attributeOldValue: true })
+
+  mediaQuery.addEventListener('change', updateThemeWithoutTransitions)
+  window.addEventListener('storage', updateThemeWithoutTransitions)
+`;
+
+export default function Document() {
+  return (
+    <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
+        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+      </Head>
+      <body className="bg-white dark:bg-neutral-900 dark:text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

f030e9ca9ba50d237cdff1f1f6d7497d0947d013
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-27 20:41:52 +0200

    refactor: change mock projects to actual real projects, make links taget blank

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,33 +3,23 @@ import Head from "next/head";
 const projects = [
   {
     id: 1,
-    title: "Project 1",
+    title: "Random Movie Generator",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque eros sit amet libero interdum, in eleifend est tincidunt. Etiam pretium tincidunt ligula ac aliquet. ",
-    image: "/project-1.jpg",
-    stack: ["React", "Next.js", "TypeScript"],
-    link: "#",
-    githubLink: "#",
+      "A random movie generator that allows you to generate a random movie, and then save it to your movie list which is stored in localStorage, project created with the Tmdb API",
+    image: "/random-movie-generator.png",
+    stack: ["React", "Remix", "TailwindCSS"],
+    link: "https://random-movie-generator.vercel.app/",
+    githubLink: "https://github.com/webmaek/random-movie",
   },
   {
     id: 2,
-    title: "Project 2",
+    title: "Recipe Smith",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque eros sit amet libero interdum, in eleifend est tincidunt. Etiam pretium tincidunt ligula ac aliquet. ",
-    image: "/project-2.jpg",
-    stack: ["React", "Next.js", "TypeScript"],
-    link: "#",
-    githubLink: "#",
-  },
-  {
-    id: 3,
-    title: "Project 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque eros sit amet libero interdum, in eleifend est tincidunt. Etiam pretium tincidunt ligula ac aliquet. ",
-    image: "/project-3.jpg",
-    stack: ["React", "Next.js", "TypeScript"],
-    link: "#",
-    githubLink: "#",
+      "Find a recipe meal for you, based on products you have in your fridge, search through recipes with pagination, project created withe the spoonacular API",
+    image: "/recipe-smith.png",
+    stack: ["React", "Remix", "TailwindCSS"],
+    link: "https://recipe-smith.vercel.app/",
+    githubLink: "https://github.com/webmaek/recipe-smith",
   },
 ];
 
@@ -81,12 +71,16 @@ export default function ProjectsPage() {
               <div className="flex items-center mt-4">
                 <a
                   href={project.link}
+                  target="_blank"
+                  rel="noreferrer"
                   className="px-2 py-1 rounded-xl bg-transparent hover:bg-indigo-500 border border-indigo-500 text-gray-700 dark:text-gray-200 hover:text-white transition-colors duration-700 ease-in-out"
                 >
                   Live
                 </a>
                 <a
                   href={project.githubLink}
+                  target="_blank"
+                  rel="noreferrer"
                   className="ml-2 px-2 py-1 rounded-xl bg-transparent hover:bg-indigo-500 border border-indigo-500 text-gray-700 dark:text-gray-200 hover:text-white transition-colors duration-700 ease-in-out"
                 >
                   Github
